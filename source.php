50f46f8a5422f79e2a4ad6c6ab430f0e86e55c3f
MissVeronica <veronica@tranza.se>
2023-09-03 01:19:41 +0200

    Create source.php

---
+++ source.php
@@ -0,0 +1,9 @@
+<?php
+add_filter( 'um_extend_field_classes', 'um_datepicker_replace_classes', 10, 3 );
+
+function um_datepicker_replace_classes( $classes, $key, $data ) {
+
+    $classes = str_replace( array( 'um-datepicker ', 'um-timepicker ' ), 
+                            array( 'walcf7-datepicker', 'walcf7-timepicker' ), $classes );
+    return $classes;
+}
